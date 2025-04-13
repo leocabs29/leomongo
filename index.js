41256d39156e1fbba2f29c6ee38b7bf7bb430728
leocabs29 <152485105+leocabs29@users.noreply.github.com>
2025-04-13 14:33:37 +0800

    finals na to

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const port = process.env.PORT || 3000;
 
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:5173','https://globalchatleocabs.netlify.app/'
+  origin: ['http://localhost:5173', 'https://globalchatleocabs.netlify.app']
 }));
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
